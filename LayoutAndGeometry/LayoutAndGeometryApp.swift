a645a526dfc4fd5ac902afd783c26962ea9f33c5
fauzandp <fauzan27dp@gmail.com>
2023-08-03 20:41:57 +0700

    Initial Commit

---
+++ LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Fauzan Dwi Prasetyo on 03/08/23.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
